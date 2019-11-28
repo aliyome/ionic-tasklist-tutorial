75d860b1450810b764693efa6f86db76241fbe4f
aliyome <aliyome@gmail.com>
2019-11-29 02:46:55 +0900

    feat: Show action sheet on touching task list

--- src/app/task-list/task-list.page.ts
+++ src/app/task-list/task-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-task-list',
@@ -10,11 +11,43 @@ export class TaskListPage {
   tasks: { name: string }[] = [{ name: 'タスク1' }, { name: 'タスク2' }];
   task: string;
 
-  constructor() {}
+  constructor(private readonly actionSheetController: ActionSheetController) {}
 
   ionViewWillEnter() {
     if ('tasks' in localStorage) {
       this.tasks = JSON.parse(localStorage.tasks);
     }
+  }
+
+  async changeTask() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          icon: 'trash',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+        },
+        {
+          text: '変更',
+          icon: 'create',
+          handler: () => {
+            console.log('Archive clicked');
+          },
+        },
+        {
+          text: '閉じる',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    actionSheet.present();
   }
 }
