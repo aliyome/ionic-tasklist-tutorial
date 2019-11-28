483620b41d95cfcddb014be3e0b1b2c5e63be3ba
aliyome <aliyome@gmail.com>
2019-11-29 02:18:02 +0900

    feat: Implement Add button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,13 @@ export class HomePage {
   title = 'タスク登録';
   tasks: { name: string }[] = [{ name: 'タスク1' }, { name: 'タスク2' }];
   task: string;
+
   constructor() {}
+
+  addTask() {
+    this.tasks.push({
+      name: this.task,
+    });
+    this.task = '';
+  }
 }
